733b29209a3cc4e2ea0f2129648e266618865c40
joshua-miles <joshua@christetulit.org>
2019-03-08 13:35:03 -0600

    initial

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class App
+  def call(environment_hash)
+
+    status_code = 500
+    header = {}
+    body = ['
+      
+    '] 
+
+    return [status_code, header, body]
+  end
+end
+
+
+
+
+
+
+
